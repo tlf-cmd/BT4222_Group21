b3bff96f5ce81088c282e3a1c66511277dfc0fb0
Yong Ler <tyongler.tang@gmail.com>
2023-03-31 18:58:45 +0800

    Tidy up folder

--- test.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-tokenizer = AutoTokenizer.from_pretrained("nbroad/ESG-BERT")
-
-model = AutoModelForSequenceClassification.from_pretrained("nbroad/ESG-BERT")
